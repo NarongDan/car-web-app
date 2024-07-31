cbd875758b6629df81ad935a310b261ea7f8b53c
NarongDan <narong.dan1@gmail.com>
2024-07-31 13:00:27 +0700

    create auth and car routes

---
+++ api/src/routes/car-route.js
@@ -0,0 +1,4 @@
+const express = require("express");
+const carRouter = express.Router();
+
+module.exports = carRouter;
--- api/src/server.js
+++ api/src/server.js
@@ -4,14 +4,21 @@ const cors = require("cors");
 const app = express();
 const errorMiddleware = require("./middlewares/error");
 const notFoundMiddleware = require("./middlewares/not-found");
-
+const authRouter = require("./routes/auth-route");
+const carRouter = require("./routes/car-route");
+//
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+// routes
 
+app.use("/auth", authRouter);
+app.use("/car", carRouter);
+//  Not Found and Error Middlewares
 app.use(notFoundMiddleware);
 app.use(errorMiddleware);
 
+//Server
 const port = process.env.PORT || 8000;
 
 app.listen(port, console.log("This server is running on port:", port));
